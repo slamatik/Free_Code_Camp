9e8b492507c188c706a297e543cc1f990b8f6f5f
slamatik <vjaceslavsmatess@gmail.com>
2020-10-13 17:59:12 +0100

    budget app first commit

---
+++ Budget App/budget.py
@@ -0,0 +1,13 @@
+class Category:
+    def __init__(self):
+        pass
+
+    def deposit(self, amount, description):
+        pass
+
+    def withdraw(self):
+
+
+
+
+def create_spend_chart(categories):
