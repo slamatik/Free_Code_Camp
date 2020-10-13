17b0e7c0c390a2d5ae958adafd46a283116346b6
slamatik <vjaceslavsmatess@gmail.com>
2020-10-13 16:34:31 +0100

    Polygon Area Calculator Completed

--- Polygon Area Calculator/shape_calculator.py
+++ Polygon Area Calculator/shape_calculator.py
@@ -32,12 +32,12 @@ class Rectangle:
 
     def get_picture(self):
         if self.width > 50 or self.height > 50:
-            return "Too big for picture"
+            return "Too big for picture."
         else:
             return f"{'*' * self.width}\n" * self.height
 
-    def get_amount_inside(self):
-        pass
+    def get_amount_inside(self, shape):
+        return self.width // shape.width * self.height // shape.height
 
     def __str__(self):
         return f"Rectangle(width={self.width}, height={self.height})"
@@ -46,37 +46,37 @@ class Rectangle:
 class Square(Rectangle):
     def __init__(self, side):
         super().__init__(width=side, height=side)
-        self.side = side
 
     def set_width(self, width):
-        self.side = width
         self.width = width
         self.height = width
 
     def set_height(self, height):
-        self.side = height
         self.width = height
         self.height = height
 
     def set_side(self, side):
-        self.side = side
         self.width = side
         self.height = side
 
     def __str__(self):
-        return f"Square(side={self.side})"
+        return f"Square(side={self.width})"
 
 
-rect = Rectangle(10, 5)
-print(rect.get_area())
-rect.set_height(3)
-print(rect.get_perimeter())
-print(rect)
-print(rect.get_picture())
-
-sq = Square(9)
-print(sq.get_area())
-sq.set_side(4)
-print(sq.get_diagonal())
-print(sq)
-print(sq.get_picture())
+# rect = Rectangle(10, 5)
+# print(rect.get_area())
+# rect.set_height(3)
+# print(rect.get_perimeter())
+# print(rect)
+# print(rect.get_picture())
+#
+# sq = Square(9)
+# print(sq.get_area())
+# sq.set_side(4)
+# print(sq.get_diagonal())
+# print(sq)
+# print(sq.get_picture())
+#
+# rect.set_height(8)
+# rect.set_width(16)
+# print(rect.get_amount_inside(sq))
