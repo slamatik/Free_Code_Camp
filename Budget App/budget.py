0e3eec76857fb70f2177ede089c5bf9ce4ae58c4
slamatik <vjaceslavsmatess@gmail.com>
2020-10-14 19:38:36 +0100

    time calculator solution

--- Budget App/budget.py
+++ Budget App/budget.py
@@ -1,13 +1,71 @@
 class Category:
-    def __init__(self):
-        pass
+    def __init__(self, category):
+        self.category = category
+        self.balance = 0
+        self.ledger = []
 
-    def deposit(self, amount, description):
-        pass
+    def deposit(self, amount, description=""):
+        self.balance += amount
+        self.ledger.append({"amount": amount, "description": description})
 
-    def withdraw(self):
+    def withdraw(self, amount, description=""):
 
+        if self.check_funds(amount):
+            self.balance -= amount
+            self.ledger.append({"amount": -amount, "description": description})
+            return True
+        else:
+            return False
 
+    def get_balance(self):
+        return self.balance
+
+    def transfer(self, amount, category):
+        if self.check_funds(amount):
+            self.withdraw(amount, f"Transfer to {category.category}")
+            self.balance -= amount
+            category.deposit(amount, f"Transfer from {self.category}")
+            return True
+        else:
+            return False
+
+    def check_funds(self, amount):
+        if amount > self.balance:
+            return False
+        else:
+            return True
+
+    def __str__(self):
+        title_line = "*" * ((30 - len(self.category)) // 2) + self.category + "*" * ((30 - len(self.category)) // 2)
+        spending = []
+        for stuff in self.ledger:
+            temp_spendings = ""
+            amount = '{0:.2f}'.format(stuff['amount'])
+            description = stuff['description'][:23]
+
+            temp_spendings += description
+            temp_spendings += " " * (30 - len(description) - len(amount))
+            temp_spendings += amount
+
+            spending.append(temp_spendings)
+
+        return title_line + "\n" + "\n".join(spending) + "\n" + f"Total: {self.balance}"
 
 
 def create_spend_chart(categories):
+    title = "Percentage spent by category"
+
+
+food = Category("Food")
+entertainment = Category("Entertainment")
+business = Category("Business")
+
+food.deposit(900, "deposit")
+entertainment.deposit(900, "deposit")
+business.deposit(900, "deposit")
+food.withdraw(105.55)
+entertainment.withdraw(33.40)
+business.withdraw(10.99)
+print(create_spend_chart([food, entertainment, business]))
+expected = "Percentage spent by category\n100|          \n 90|          \n 80|          \n 70|    o     \n 60|    o     \n 50|    o     \n 40|    o     \n 30|    o     \n 20|    o  o  \n 10|    o  o  \n  0| o  o  o  \n    ----------\n     B  F  E  \n     u  o  n  \n     s  o  t  \n     i  d  e  \n     n     r  \n     e     t  \n     s     a  \n     s     i  \n           n  \n           m  \n           e  \n           n  \n           t  "
+print(expected)
--- Time Calculator/time_calculator.py
+++ Time Calculator/time_calculator.py
@@ -1,7 +1,68 @@
-def add_time(start, duration):
+def add_time(start, duration, day=None):
+    answer = ""
+
+    week_days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+    half = start[-2:]
+
+    current_time = start[:-3]
+    current_min = int(current_time[-2:])
+    current_hours = int(current_time[:-3])
+
+    to_add_min = int(duration[-2:])
+    to_add_hours = int(duration[:-3])
+    new_days = 0
+
+    minutes_half = False
+
+    while to_add_min > 0:
+        current_min += 1
+        to_add_min -= 1
+        if current_min >= 60:
+            current_hours += 1
+            current_min -= 60
+        if current_hours == 12 and not minutes_half:
+            half = change_half(half)
+            minutes_half = True
+            if half == "AM":
+                new_days += 1
+        if current_hours > 12:
+            current_hours -= 12
+
+    while to_add_hours > 0:
+        current_hours += 1
+        to_add_hours -= 1
+        if current_hours == 12:
+            half = change_half(half)
+            if half == "AM":
+                new_days += 1
+        if current_hours > 12:
+            current_hours -= 12
+
+    current_hours = str(current_hours)
+    current_min = str(current_min)
+    if len(current_min) != 2:
+        current_min = "0" + current_min
+
+    answer = current_hours + ":" + current_min + " " + half
+
+    if day:
+        day_index = week_days.index(day.lower())
+        new_day_index = new_days + day_index
+        while new_day_index > 7:
+            new_day_index %= 7
+        answer += f", {week_days[new_day_index].capitalize()}"
+
+    if new_days == 1:
+        answer += " (next day)"
+
+    if new_days > 1:
+        answer += f" ({new_days} days later)"
+
+    return answer
 
 
-
-
-
-    return new_time
+def change_half(half):
+    if half == "AM":
+        return "PM"
+    else:
+        return "AM"
